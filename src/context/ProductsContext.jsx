d263c5ea7d77e3de1ae0f5dfca452f8928258e8d
sajjad nazari dev <sajadnazari269@gmail.com>
2025-02-27 12:40:24 +0330

    [add] Implemented ProductsContext for managing product data

---
+++ src/context/ProductsContext.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { createContext } from "react";
+import { useState } from "react";
+import api from "../services/config";
+import PropTypes from "prop-types";
+
+const ProductsContext = createContext();
+
+function ProductsProvider({ children }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setProducts(await api.get("/products"));
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductsContext.Provider value={{ products }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+}
+
+ProductsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ProductsProvider;
