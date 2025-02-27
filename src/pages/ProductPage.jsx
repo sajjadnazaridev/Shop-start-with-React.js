6e754fdba7df481922b5d03fa3463769f6bb3d0f
sajjad nazari dev <sajadnazari269@gmail.com>
2025-02-27 16:45:12 +0330

    [add] Implement Loader component and integrate it into ProductPage for loading state

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,4 +1,5 @@
 import CardProduct from "../components/CardProduct";
+import Loader from "../components/ui/Loader";
 import useProducts from "../hooks/useProducts";
 
 function ProductPage() {
@@ -7,7 +8,7 @@ function ProductPage() {
   return (
     <div className="flex justify-between">
       <div className="flex flex-wrap justify-between w-full">
-        {!products.length && <h1>No products</h1>}
+        {!products.length && <Loader />}
         {products.map((product) => (
           <CardProduct key={product.id} data={product} />
         ))}
