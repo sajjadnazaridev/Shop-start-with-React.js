f42b8caf285acd6264e973e8602ae5569aa007e4
sajjad nazari dev <sajadnazari269@gmail.com>
2025-02-27 15:57:46 +0330

    [add] Implement ProductPage and CardProduct component for displaying product details

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,9 +1,20 @@
+import CardProduct from "../components/CardProduct";
 import useProducts from "../hooks/useProducts";
 
 function ProductPage() {
   const products = useProducts();
   console.log(products);
-  return <div>ProductPage</div>;
+  return (
+    <div className="flex justify-between">
+      <div className="flex flex-wrap justify-between w-full">
+        {!products.length && <h1>No products</h1>}
+        {products.map((product) => (
+          <CardProduct key={product.id} data={product} />
+        ))}
+      </div>
+      <div>Sidebar</div>
+    </div>
+  );
 }
 
 export default ProductPage;
