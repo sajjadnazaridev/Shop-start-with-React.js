6e3f22f1bee6a650bdc60a712e5786e0ecca354e
sajjad nazari dev <sajadnazari269@gmail.com>
2025-03-01 01:39:38 +0330

    [add] Integrate search functionality in ProductPage with input and button using react-icons

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,20 +1,44 @@
+import { useState } from "react";
 import CardProduct from "../components/CardProduct";
 import Loader from "../components/ui/Loader";
 import useProducts from "../hooks/useProducts";
+import { ImSearch } from "react-icons/im";
 
 function ProductPage() {
+  const [search, setSearch] = useState();
+  const [resultSearch, setResultSearch] = useState("");
   const products = useProducts();
-  console.log(products);
+
+  const searchHandler = () => {
+    console.log(search);
+  };
+
   return (
-    <div className="flex justify-between">
-      <div className="flex flex-wrap justify-between w-full">
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <CardProduct key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={resultSearch}
+          onChange={(e) => {
+            setSearch(e.target.value.toLowerCase().trim());
+            setResultSearch(e.target.value);
+          }}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>Sidebar</div>
-    </div>
+      <div className="flex justify-between">
+        <div className="flex flex-wrap justify-between w-full">
+          {!products.length && <Loader />}
+          {products.map((product) => (
+            <CardProduct key={product.id} data={product} />
+          ))}
+        </div>
+        <div>Sidebar</div>
+      </div>
+    </>
   );
 }
 
